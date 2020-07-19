75f46b18d8ddda0230ea3b436dfeae88f0dd0c0d
= <khangie@hotmail.com>
2020-07-18 17:22:37 -0700

    first commit

---
+++ src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -0,0 +1,22 @@
+package io.javabrains.moviecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MovieCatalogServiceApplication {
+
+	// Make RestTemplate a singleton to be injected into other classes
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogServiceApplication.class, args);
+	}
+
+}
