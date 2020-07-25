bd656bf1356b98438919b84e85cb6901d47cce46
= <khangie@hotmail.com>
2020-07-25 14:12:25 -0700

    hystrix config

--- src/main/java/io/javabrains/moviecatalogservice/resource/MovieCatalogResource.java
+++ src/main/java/io/javabrains/moviecatalogservice/resource/MovieCatalogResource.java
@@ -5,6 +5,8 @@ import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
 import io.javabrains.moviecatalogservice.models.UserRating;
+import io.javabrains.moviecatalogservice.services.MovieInfo;
+import io.javabrains.moviecatalogservice.services.UserRatingInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,26 +25,47 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    MovieInfo movieInfo;
+
+    @Autowired
+    UserRatingInfo userRatingInfo;
+
     @RequestMapping("/{userId}")
-    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
+        UserRating ratings = userRatingInfo.getUserRating(userId);
 
-        return ratings.getUserRating().stream().map(rating -> {
-
-            // For each movieId, call movie service and get details
-            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-
-            // Put them all together
-            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
-
-        }).collect(Collectors.toList());
-
+        return ratings.getUserRating().stream()
+                .map(rating -> movieInfo.getCatalogItem(rating))
+                .collect(Collectors.toList());
     }
 
-    public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
-        return Arrays.asList(new CatalogItem("No movie", "", 0));
+    /*
+    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+    private CatalogItem getCatalogItem(Rating rating) {
+        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+        return new CatalogItem(movie.getName(), "Desc", rating.getRating());
     }
+
+    private CatalogItem getFallbackCatalogItem(Rating rating) {
+        return new CatalogItem("Movie name not found", "Desc", rating.getRating());
+    }
+
+    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
+    private UserRating getUserRating(@PathVariable("userId") String userId) {
+        return restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
+    }
+
+    private UserRating getFallbackUserRating(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(Arrays.asList(
+                new Rating("0", 0)
+        ));
+        return userRating;
+    }
+
+
+     */
 
 }
